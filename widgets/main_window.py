cfc688c724925afaeaee3a5ffe2f19f014dd6df1
abeshahsan <abeshahsan2002@gmail.com>
2023-12-18 20:33:39 +0600

    integrating blur

--- widgets/main_window.py
+++ widgets/main_window.py
@@ -1,3 +1,5 @@
+import copy
+
 from PyQt6 import uic
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
@@ -6,34 +8,46 @@ from filepaths import Filepaths
 from utilites import ValueProperty, pixmap_to_numpy, numpy_to_pixmap
 from widgets.crop_rubberband_widget import CropRubberBandWidget
 from widgets.adjust_widget import UI_AdjustWidget
+import image_operations
 
 
 class UI_MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.adjust_widget = None
-        self.save_file_path = None
         uic.loadUi(Filepaths.MAIN_WINDOW(), self)
         self.setWindowTitle('Photo Wizard')
         # self.setFixedSize(800, 600)
 
-        """The canvas is to hold the image to be shown on the screen."""
+        """Loading necessary objects from the loaded ui."""
+        # The canvas is to hold the image to be shown on the screen.
         self.canvas = self.findChild(QGraphicsView, 'canvas')
         self.editor_container = self.findChild(QHBoxLayout, 'editor_container')
         self.cancel_button = self.findChild(QPushButton, 'cancel_button')
 
         self.scene = QGraphicsScene()
         self.canvas.setScene(self.scene)
+
+        """Some necessary variables needed for canvas. Initializing with None now. will need later."""
+        self.adjust_widget = None
+        self.save_file_path = None
         self.pixmap = None
+        self.original_image = None
+        self.scene_image = None
+        self.crop_rubber_band = None
 
         self.load_adjust_widget()
 
+        """Some event handlers needed for different operations."""
         self.action_open.triggered.connect(self.open_image)
         self.action_save_as.triggered.connect(self.save_new_file)
         self.action_save.triggered.connect(self.save_file)
         # self.cancel_button.clicked.connect(self.load_adjust_widget)
 
     def choose_file(self):
+        """
+        Opens a file dialog. lets the user choose a file to open and returns the path of the file.
+        :return: the path of the selected file.
+        """
         file_dialogue = QFileDialog(self)
         filters = "Images (*.jpg *.png *.bmp)"
         filenames, _ = file_dialogue.getOpenFileNames(self, filter=filters)
@@ -42,9 +56,18 @@ class UI_MainWindow(QMainWindow):
         return filenames[0]
 
     def open_image(self):
+        """
+        Clicking 'Open' or pressing Ctrl+O \n
+        Opens an image file and loads it into the screen.\n
+        * Opens a file dialog. Using the choose_file() method.
+        * If you select an image file, it loads it into the screen.
+        :return:
+        """
         image_file_path = self.choose_file()
 
         self.pixmap = QPixmap(image_file_path)
+        self.original_image = self.pixmap.toImage().copy()
+        self.scene_image = self.original_image.copy()
         if self.pixmap is not None and not self.pixmap.isNull():
             self.scale_pixmap()
             self.scene = QGraphicsScene()
@@ -65,9 +88,9 @@ class UI_MainWindow(QMainWindow):
         So a file-dialogue will open to get the directory and the filename.
         :return:
         """
-        file_dialogue = QFileDialog()
+        file_dialogue = QFileDialog(self)
         filters = "Images (*.jpg *.png *.bmp)"
-        file_path, _ = file_dialogue.getSaveFileName(filter=filters)
+        file_path, _ = file_dialogue.getSaveFileName(filter=filters, parent=self)
         self.save_file_path = file_path
         if file_path:
             self.pixmap.save(file_path)
@@ -102,7 +125,7 @@ class UI_MainWindow(QMainWindow):
         return self.pixmap
 
     def update_scene(self):
-        self.pixmap = self.crop_pixmap()
+        self.pixmap = QPixmap(self.scene_image)
         self.scale_pixmap()
         self.scene = QGraphicsScene()
         self.scene.addPixmap(self.pixmap)
@@ -118,6 +141,10 @@ class UI_MainWindow(QMainWindow):
         self.crop_rubber_band = CropRubberBandWidget(self.canvas)
         self.crop_rubber_band.setGeometry(0, 0, self.width(), self.height())
         self.crop_rubber_band.show()
+
+    def blur(self):
+        self.scene_image = image_operations.blur_image(self.original_image)
+        self.update_scene()
 
 
 if __name__ == "__main__":
